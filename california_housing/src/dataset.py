de218fcc86f7e706ca8acc8b8f55956ff48e4943
RudenkoBogdan <rudenko.ba03@gmail.com>
2026-03-29 20:10:35 +0300

    feat(dataset.py): изменены названия методов, добавлен метод возврата таргета

--- california_housing/src/dataset.py
+++ california_housing/src/dataset.py
@@ -2,9 +2,14 @@ import polars as pl
 from sklearn.datasets import fetch_california_housing
 
 class Data:
-    def __init__(self):
-        data = fetch_california_housing().data
-        self.df = pl.DataFrame(data)
+    def __init__(self, target="target"):
+        data = fetch_california_housing()
+        df = pl.DataFrame(data.data, schema=data.feature_names)
+        self.df = df.with_columns(pl.Series(target, data.target))
+        self.target = target
         
-    def load(self):
-        return self.df
+    def get_features(self):
+        return self.df.select(pl.all().exclude(self.target))
+    
+    def get_target(self):
+        return self.df.select(self.target)
